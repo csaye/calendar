7dba35d935c482579b41c98b7dcd2571b8a3c355
Cooper Saye <coopersaye@gmail.com>
2021-05-02 19:48:58 -0600

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,27 @@
 import Calendar from '../Calendar/Calendar.js';
+import SignIn from '../SignIn/SignIn.js';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 import './App.css';
 
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+  useAuthState(firebase.auth());
+
   return (
     <div className="App">
-      <Calendar />
+      {
+        firebase.auth().currentUser ?
+        <Calendar /> :
+        <SignIn />
+      }
     </div>
   );
 }
