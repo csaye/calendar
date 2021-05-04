0eba5b278fbcb431990f43fd40132e617158c601
Cooper Saye <coopersaye@gmail.com>
2021-05-03 18:37:10 -0600

    Implement month switching.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,28 +1,49 @@
+import React, { useState } from 'react';
+
 import Day from '../Day/Day.js';
+import Header from '../Header/Header.js';
 
 import './Calendar.css';
 
 function Calendar() {
   const now = new Date();
-  const month = now.getMonth();
-  const year = now.getFullYear();
+  const [year, setYear] = useState(now.getFullYear());
+  const [month, setMonth] = useState(now.getMonth());
+
+  // moves calendar back one month
+  function backMonth() {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  }
+
+  // moves calendar forward one month
+  function forwardMonth() {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  }
 
   // returns number of days in the current month
   function daysInMonth() {
-    const year = now.getFullYear();
-    const nextMonth = now.getMonth() + 1;
+    const nextMonth = month + 1;
     const endOfMonth = new Date(year, nextMonth, 0);
     return endOfMonth.getDate();
   }
 
   // returns the offset of the current month
   function monthOffset() {
-    const year = now.getFullYear();
-    const month = now.getMonth();
     const firstOfMonth = new Date(year, month, 1);
     return firstOfMonth.getDay();
   }
 
+  // initialize calendar boxes
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   const fillers = [];
   const days = [];
@@ -31,19 +52,23 @@ function Calendar() {
 
   return (
     <div className="Calendar">
+      <Header year={year} month={month} backMonth={backMonth} forwardMonth={forwardMonth} />
       <div className="month">
         {
+          // day names
           dayNames.map(n =>
               <div key={`dayname-${n}`} className="title-box calendar-box">{n}</div>
           )
         }
         {
+          // fillers
           fillers.map(f =>
             <span key={`filler-${f}`} className="calendar-box" />
           )
         }
         {
-          days.map(day =>
+          // days
+          days && days.map(day =>
             <Day key={`day-${day}`} date={new Date(year, month, day)} />
           )
         }
